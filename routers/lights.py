d3da60362561dfe5e8acbf54b2f8e1ffd9cd3aba
Rick <rick@heddema.org>
2020-11-22 21:26:09 +0100

    Update 2.5

---
+++ routers/lights.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, render_template, request, redirect, session
+from utils import light_handler
+
+lightRoute = Blueprint("lights", __name__)
+
+@lightRoute.route('/hue/<light>/<hue>', methods=["POST"])
+def hue_route(light, hue):
+    light_handler.change_light(light=str(light), on=True, hue=int(hue))
+    resp = {
+        "error": False, "output":"None"
+    }
+    return resp
+
+@light_handler.route('/bri/<light>/<brightness>', methods=["POST"])
+def bri_route(light, brightness):
+    light_handler.change_light(light=str(light), on=True, brightness=int(brightness))
+    resp = {
+        "error": False, "output":"None"
+    }
+    return resp
